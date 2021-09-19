de5fe0ff54e3b0b1e4b090697b26ae6c8539f550
Paul J. Iutzi <pji@mac.com>
2021-09-18 20:38:13 -0500

    Removed the star imports from imggen.

--- imggen/__init__.py
+++ imggen/__init__.py
@@ -4,9 +4,4 @@ __init__
 
 Initialization for the imggen package.
 """
-from imggen.maze import *
-from imggen.noise import *
-from imggen.patterns import *
-from imggen.perlin import *
-from imggen.unitnoise import *
-from imggen.worley import *
+__all__ = ['maze', 'noise', 'patterns', 'perlin', 'unitnoise', 'worley']
